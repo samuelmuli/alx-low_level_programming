3af648ca0e9da6742bab4889f011f525be25c683
samuel <samuelmuli045@gmail.com>
2023-03-18 01:13:52 +0300

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * return: return zero
+ */
+
+int main(void)
+{
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
