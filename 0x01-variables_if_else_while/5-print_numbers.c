055ba4b03036ba70406df0f2953b02c1176c97f3
samuel <samuelmuli045@gmail.com>
2023-03-18 02:31:36 +0300

    0-9 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: return zero
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	return (0);
+}
