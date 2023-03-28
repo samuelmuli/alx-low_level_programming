54fbe47a93406665697823213b12c6c07d455a2b
samuel <samuelmuli045@gmail.com>
2023-03-28 19:25:30 +0300

    first c pointer encounter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *reset_to_98 - function that takes int as parameter
+ *
+ *@n: parameter passed
+ *
+ * Return: 0 (success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
