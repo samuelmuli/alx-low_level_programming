943ad3d080f88013a2ab10152c8a758d6f523b70
samuel <samuelmuli045@gmail.com>
2023-04-06 14:55:01 +0300

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,13 +6,13 @@
  *
  *@n: parameter
  *
- *Return: 0 success
+ *Return: voidi
  */
 void reverse_array(int *a, int n)
 {
 	int x, y = 0;
 
-	for (x = 0; x < n--, x++)
+	for (x = 0; x < n--; x++)
 	{
 		y = a[x];
 		a[x] = a[n];
