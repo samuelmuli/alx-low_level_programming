00e99091a19113dc18c21490c089a24c7f040204
samuel <samuelmuli045@gmail.com>
2023-04-06 17:05:19 +0300

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*_strstr - function that locates a substring
+*@haystack: parameter
+*@needle: parameter
+*Return: 0 success
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *x = haystack;
+		char *y = needle;
+
+		while (*x == *y && *y != '\0')
+		{
+			x++;
+			y++;
+		}
+		if (*y == '\0')
+			return (haystack);
+	}
+	return (0);
+}
