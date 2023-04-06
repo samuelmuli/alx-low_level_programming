77e1956d607b55d465621d91e4e841abed2f87df
samuel <samuelmuli045@gmail.com>
2023-04-06 14:36:42 +0300

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strcmp - compare strings
+ *
+ *@s1: parameter
+ *
+ *@s2: parameter
+ *
+ *Return: 0 success
+ */
+int _strcmp(char *s1, char *s2);
+{
+	int x = 0;
+
+	while (s1[x] != '\0' && s2[x] != '\0')
+	{
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
+		x++;
+	}
+	return (0);
+}
