7a62e21b69a76f842868210bf40880f269e992cd
samuel <samuelmuli045@gmail.com>
2023-04-06 15:39:36 +0300

    encoding a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *leet - function that encodes 1337
+ *
+ *@n: parameter
+ *
+ *Return: 0 success
+ */
+char *leet(char *n);
+{
+	int x, y;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (x = 0; n[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (n[x] == s1[y])
+			{
+				n[x] = s2[y];
+			}
+		}
+	}
+	return (n)
+}
